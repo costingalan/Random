cc018c371e46f6c67b30ff8ffd60d2edd7e7bf91
Costin Galan <galan.costin@gmail.com>
2015-09-08 17:09:09 +0300

    Add sshd reload

--- bash_scripts/secure_remote.sh
+++ bash_scripts/secure_remote.sh
@@ -6,20 +6,20 @@
 echo $OS
 
 if [[ $OS == "CentOS" ]]; then
-	echo "Good";
 	sudo yum install epel-release -y;
 	sudo yum install fail2ban -y;
 
 	sed -i s/"maxretry = 5"/"maxretry = 8"/ /etc/fail2ban/jail.conf
 	sed -i s/"#PermitRootLogin"/"PermitRootlogin"/g /etc/ssh/sshd_config
 	sed -i s/"PermitRootLogin yes"/"PermitRootlogin no"/g /etc/ssh/sshd_config
+    sudo service reload sshd
 fi
 
 if [[ $OS == "Ubuntu" ]]; then
-	echo "Not good";
 	sudo apt-get install fail2ban -y;
 
 	sed -i s/"maxretry = 5"/"maxretry = 8"/ /etc/fail2ban/jail.conf
 	sed -i s/"#PermitRootLogin"/"PermitRootlogin"/g /etc/ssh/sshd_config
 	sed -i s/"PermitRootLogin yes"/"PermitRootLogin no"/g /etc/ssh/sshd_config
+    sudo service ssh restart
 fi
