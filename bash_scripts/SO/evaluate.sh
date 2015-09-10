7489e337a93028e902f5c57f8b397cb99e6cd7f9
Costin Galan <galan.costin@gmail.com>
2015-09-10 19:12:04 +0300

    Added the evaluate script
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

---
+++ bash_scripts/SO/evaluate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#6.1. Folosind comanda eval sa se scrie un program, care afiseaza argumentele din
+#linia de comanda in ordine inversa. Se considera cazul in care programul primeste
+#cel mult 9 argumente in linia de comanda.
+
+if [[ "$#" -gt 9 ]]; then
+   echo "Too many arguments"
+   exit 1
+fi
+
+for i in $(seq $# -1 1); do 
+eval "echo \$$i"
+done
