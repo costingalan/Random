d9c9d32193274b2de5ed928658047a4a8ece40bc
Costin Galan <galan.costin@gmail.com>
2015-09-16 17:18:07 +0300

    Changed sshd config file for ubuntu
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

--- bash_scripts/assignements/secure_remote.sh
+++ bash_scripts/assignements/secure_remote.sh
@@ -36,6 +36,7 @@ if [[ $OS == "Ubuntu" ]]; then
 	sed -i s/"maxretry = 5"/"maxretry = 8"/ /etc/fail2ban/jail.conf
 	sed -i s/"#PermitRootLogin"/"PermitRootlogin"/g /etc/ssh/sshd_config
 	sed -i s/"PermitRootLogin yes"/"PermitRootLogin no"/g /etc/ssh/sshd_config
+	sed -i s/"PermitRootLogin without-password"/"PermitRootLogin no"/g /etc/ssh/sshd_config
   
   sudo service ssh reload; 
   sudo service fail2ban restart; #needed after changing the config file 
