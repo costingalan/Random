526be523c344f73f7e1e95ea257bf6bb2135494b
Costin Galan <cgalan@cloudbasesolutions.com>
2015-10-08 12:43:58 +0300

    Added /bin/true

--- bash_scripts/assignements/better_unstack.sh
+++ bash_scripts/assignements/better_unstack.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-sudo killall --ignore-case -9 "/usr/bin/python"   
-sudo killall --ignore-case -9 "python" 
-sudo killall --ignore-case -9 "SCREEN"
-sudo killall --ignore-case -9 "nova-api" 
-sudo killall --ignore-case -9 "cinder-api"
-sudo killall --ignore-case -9 "neutron-api"
-sudo killall --ignore-case -9 "glance-api"
-sudo killall --ignore-case -9 "quantum"
-sudo killall --ignore-case -9 "heat-api"
-sudo killall --ignore-case -9 "rabbitmq"
-sudo killall --ignore-case -9 "dstat"
-sudo killall --ignore-case -9 "keystone"
-sudo killall --ignore-case -9 "ovsdb-server"
-sudo killall --ignore-case -9 "ovs-vswitchd"
-sudo killall --ignore-case -9 "apache2"
+#added || /bin/true because killall on an nonexistent process return an error
+
+sudo killall --ignore-case -9 "/usr/bin/python" || /bin/true  
+sudo killall --ignore-case -9 "python"  || /bin/true
+sudo killall --ignore-case -9 "SCREEN" || /bin/true
+sudo killall --ignore-case -9 "nova-api" || /bin/true 
+sudo killall --ignore-case -9 "cinder-api" || /bin/true
+sudo killall --ignore-case -9 "neutron-api" || /bin/true
+sudo killall --ignore-case -9 "glance-api" || /bin/true
+sudo killall --ignore-case -9 "quantum" || /bin/true
+sudo killall --ignore-case -9 "heat-api" || /bin/true
+sudo killall --ignore-case -9 "rabbitmq" || /bin/true
+sudo killall --ignore-case -9 "dstat" || /bin/true
+sudo killall --ignore-case -9 "keystone" || /bin/true
+sudo killall --ignore-case -9 "ovsdb-server" || /bin/true
+sudo killall --ignore-case -9 "ovs-vswitchd" || /bin/true
+sudo killall --ignore-case -9 "apache2" || /bin/true
