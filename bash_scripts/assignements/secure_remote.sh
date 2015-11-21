8978ef4a6d31c9d6d42407775b13d7862337623c
Costin Galan <cgalan@cloudbasesolutions.com>
2015-11-21 16:42:09 +0200

    Added explanations regarding some commands

--- bash_scripts/assignements/secure_remote.sh
+++ bash_scripts/assignements/secure_remote.sh
@@ -8,25 +8,25 @@ echo "Checking which operating system you have: ";
 [[ -f /etc/redhat-release ]] && OS=CentOS 
 [[ -f /etc/lsb-release ]] && OS=Ubuntu    
 
-echo "You have $OS";
-echo "Configuring your system...";
+echo "Configuring your system for $OS";
 
 if [[ $OS == "CentOS" ]]; then
 	
-  sudo yum install epel-release -y; #fail2ban is avaible in epel-release
-	sudo yum install fail2ban -y;
+  sudo yum update -y; #Needed for updating the system packages
+  sudo yum install epel-release -y; #Needed for installing fail2ban.
+  sudo yum install fail2ban -y;
 
-	sed -i s/"maxretry = 5"/"maxretry = 8"/ /etc/fail2ban/jail.conf
-	sed -i s/"#PermitRootLogin"/"PermitRootlogin"/g /etc/ssh/sshd_config
-	sed -i s/"PermitRootLogin yes"/"PermitRootlogin no"/g /etc/ssh/sshd_config
+  sed -i s/"maxretry = 5"/"maxretry = 8"/ /etc/fail2ban/jail.conf #increase the number of retries from 5 to 8
+  sed -i s/"#PermitRootLogin"/"PermitRootlogin"/g /etc/ssh/sshd_config
+  sed -i s/"PermitRootLogin yes"/"PermitRootlogin no"/g /etc/ssh/sshd_config
   echo "PermitRootLogin no" >> /etc/centrifydc/ssh/sshd_config
   
-  sudo systemctl reload sshd;
-  sudo systemctl restart fail2ban; #needed after changing the config file
-  sudo systemctl enable fail2ban;
-  sudo adreload
-  sudo adflush
-  kill -9 "$(pgrep -f "/usr/share/centrifydc/sbin/sshd -D")" 
+  sudo systemctl reload sshd; #reloading the sshd service
+  sudo systemctl restart fail2ban; #needed for realoading the configuration file
+  sudo systemctl enable fail2ban; #enable fail2ban at startup
+  sudo adreload #centrifydc needs to reload the AD users and OUs
+  sudo adflush  #centrifydc needs to have the AD configuration restarted after updating the sshd config file
+  kill -9 "$(pgrep -f "/usr/share/centrifydc/sbin/sshd -D")" #force kill centrifydc's sshd for updating the config
 fi
 
 if [[ $OS == "Ubuntu" ]]; then
@@ -34,10 +34,10 @@ if [[ $OS == "Ubuntu" ]]; then
   sudo apt-get install openssh-server openssh-client -y;
   sudo apt-get install fail2ban -y;
 
-	sed -i s/"maxretry = 5"/"maxretry = 8"/ /etc/fail2ban/jail.conf
-	sed -i s/"#PermitRootLogin"/"PermitRootlogin"/g /etc/ssh/sshd_config
-	sed -i s/"PermitRootLogin yes"/"PermitRootLogin no"/g /etc/ssh/sshd_config
-	sed -i s/"PermitRootLogin without-password"/"PermitRootLogin no"/g /etc/ssh/sshd_config
+  sed -i s/"maxretry = 5"/"maxretry = 8"/ /etc/fail2ban/jail.conf
+  sed -i s/"#PermitRootLogin"/"PermitRootlogin"/g /etc/ssh/sshd_config
+  sed -i s/"PermitRootLogin yes"/"PermitRootLogin no"/g /etc/ssh/sshd_config
+  sed -i s/"PermitRootLogin without-password"/"PermitRootLogin no"/g /etc/ssh/sshd_config
   echo "PermitRootLogin no" >> /etc/centrifydc/ssh/sshd_config
   
   sudo service fail2ban restart;
