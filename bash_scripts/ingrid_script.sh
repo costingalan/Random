521203195de4b93213c7681fedd082c710482191
Costin Galan <cgalan@cloudbasesolutions.com>
2016-02-07 03:38:23 +0200

    Improved ingrid_script.sh
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

--- bash_scripts/ingrid_script.sh
+++ bash_scripts/ingrid_script.sh
@@ -4,19 +4,18 @@
 # License: Apache V2.0
 # Description: Help script
 
-listing=$(ls ~/solutii_pt_costin)
-git clone https://github.com/alexandrucoman/python-lab ~/work/Cloudbase/script/python-lab
-cd ~/work/Cloudbase/script/python-lab
+targetDir=~"/work/Cloudbase/script/python-lab"
+git clone https://github.com/alexandrucoman/python-lab $targetDir
+cd ~/work/Cloudbase/script/python-lab || echo "L9: Failed to change directory"
 git remote remove origin
 git remote add origin https://github.com/IngridSt/python-lab
 git remote add alex https://github.com/alexandrucoman/python-lab
 git checkout -b python_lab
 mkdir -p solutii/IngridSt
-cd solutii/IngridSt
-for script in $listing;
-do
-    cp ~/solutii_pt_costin/$script ~/work/Cloudbase/script/python-lab/solutii/IngridSt/
-    git add $script
+cd solutii/IngridSt || echo "ERROR: L15: Failed to change directory"
+for script in ~/solutii_pt_costin/*; do
+    cp ~/solutii_pt_costin/"$script" $targetDir/solutii/IngridSt/
+    git add "$script"
     git commit -m "Adaugat solutie pentru $script"
 done
 git log
